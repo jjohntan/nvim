548b19c0dcfc7af069a46918935a08384d542062
jjohntan <john@DESKTOP-7QPDSN8>
2024-11-19 16:33:56 +0800

    set tab key to insert tab character

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,8 @@
 --
 vim.g.autoformat = false
 vim.opt.mouse = ""
+
+vim.opt.tabstop = 4        -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 4     -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 4    -- Number of spaces that a <Tab> counts for while editing
+vim.opt.expandtab = false  -- Use tab character instead of spaces
